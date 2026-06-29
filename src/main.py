69f1a465e877482b6c348f9fc8808296955fe0e3
enskyeing <skyetsatsis@gmail.com>
2026-06-28 18:33:30 -0700

    Get bot running with test cmd

--- src/main.py
+++ src/main.py
@@ -1,1 +1,30 @@
+import discord
+from discord.ext import commands
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+BOT_TOKEN = os.getenv("BOT_TOKEN") 
+if BOT_TOKEN is None:
+    raise ValueError("BOT_TOKEN is not set in the environment variables.")
+
+PREFIX = ".s"
+
+class EventBot(commands.Bot):
+    def __init__(self):
+        intents = discord.Intents.all()
+        super().__init__(command_prefix=commands.when_mentioned_or(PREFIX), intents=intents)
+
+    async def on_ready(self):
+        print("I'm ready to rumble.")
+
+bot = EventBot()
+
+@bot.command()
+@commands.is_owner()
+async def test(ctx):
+    await ctx.send(f"Hello!")
+
+bot.run(BOT_TOKEN)
 
